166d1c28a83aaeb1033c03e327eac558437bf8b2
Jamie Muir <jaamiem@users.noreply.github.com>
2018-03-28 08:22:21 +0100

    Add files via upload

---
+++ ADT/src/Week7/Main.java
@@ -0,0 +1,16 @@
+package Week7;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args){
+		Map<String, String>	capitals = new HashMap();
+		
+		capitals.put("Scotland", "Edinburgh");
+		capitals.put("England", "London");
+		capitals.put("Germany", "Berlin");
+		capitals.put("France", "Paris");
+		capitals.put("Spain", "Madrid");
+	}
+}
